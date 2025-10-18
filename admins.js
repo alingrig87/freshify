8f86b3e5d30d34c9439039eac462f3f1433f8666
Alin Grigorescu <alin.grig87@gmail.com>
2025-10-18 14:59:03 +0300

    finish admin page

--- admins.js
+++ admins.js
@@ -3,7 +3,14 @@ window.addEventListener('DOMContentLoaded', renderTable);
 const URL = 'https://670fe587a85f4164ef2c6100.mockapi.io/alimente';
 
 const tableBody = document.querySelector('#products-table tbody');
-const addBtn = document.querySelector('#add-btn');
+const addOrEditBtn = document.querySelector('#add-or-edit-btn');
+let isEditMode = false;
+let productId;
+
+const nameInput = document.getElementById('name');
+const priceInput = document.getElementById('price');
+const imageURLInput = document.getElementById('imageURL');
+const descriptionInput = document.getElementById('description');
 
 function renderTable() {
 	fetch(URL)
@@ -41,14 +48,14 @@ function renderTable() {
 		});
 }
 
-addBtn.addEventListener('click', addNewProduct);
+addOrEditBtn.addEventListener('click', addOrEditNewProduct);
 
-function addNewProduct(e) {
+function addOrEditNewProduct(e) {
 	e.preventDefault();
-	const name = document.getElementById('name').value;
-	const price = document.getElementById('price').value;
-	const imageURL = document.getElementById('imageURL').value;
-	const description = document.getElementById('description').value;
+	const name = nameInput.value;
+	const price = priceInput.value;
+	const imageURL = imageURLInput.value;
+	const description = descriptionInput.value;
 
 	const newProduct = {
 		name: name,
@@ -57,9 +64,63 @@ function addNewProduct(e) {
 		details: description,
 	};
 
-	fetch(URL, {
-		method: 'POST',
+	const method = isEditMode ? 'PUT' : 'POST';
+	const newUrl = isEditMode ? `${URL}/${productId}` : URL;
+
+	fetch(newUrl, {
+		method: method,
 		headers: { 'Content-Type': 'application/json' },
 		body: JSON.stringify(newProduct),
-	}).then((response) => renderTable());
+	}).then((response) => {
+		renderTable();
+		resetForm();
+	});
 }
+
+function resetForm() {
+	nameInput.value = '';
+	priceInput.value = '';
+	imageURLInput.value = '';
+	descriptionInput.value = '';
+
+	if (isEditMode) {
+		isEditMode = false;
+		addOrEditBtn.innerHTML = 'Add product';
+	}
+}
+
+tableBody.addEventListener('click', handleActions);
+
+function handleActions(e) {
+	const clickedElement = e.target;
+	if (clickedElement.parentElement.classList.contains('edit')) {
+		productId = getTableRow(clickedElement).dataset.id;
+		fetch(`${URL}/${productId}`)
+			.then((response) => response.json())
+			.then((product) => {
+				console.log(product);
+				nameInput.value = product.name;
+				priceInput.value = product.price;
+				imageURLInput.value = product.imageURL;
+				descriptionInput.value = product.details;
+			});
+		isEditMode = true;
+		console.log(addOrEditBtn, isEditMode);
+		addOrEditBtn.innerHTML = 'Save';
+	} else if (clickedElement.parentElement.classList.contains('delete')) {
+		productId = getTableRow(clickedElement).dataset.id;
+		fetch(`${URL}/${productId}`, {
+			method: 'DELETE',
+		}).then((response) => renderTable());
+	}
+}
+
+function getTableRow(editIcon) {
+	return editIcon.parentElement.parentElement.parentElement.parentElement;
+}
+
+// buton de add product, se transform in add or edit si schimbam id-ul
+// cream o variabila de mod edit in care stocam true daca editam sau false daca adaugam(default value)
+// in momentul in care punem in input datele dintr-un produs care urmeaza sa fie editat, atunci variabila de edit mode se duce la true si i se schimba continutul din add product in save
+// metodele si numele de variabile pentru addNewProduct se transorma in ceva care se duca cu gandul ca si editam, exemplu: addOrEditBtn
+// la metoda care facea post trebuie sa adaugam o variabila method care va fi fie POST fie PUT in functie de valoarea lui isEditMode folosind ternary operator.
